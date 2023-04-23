2fbcfc1311d8541272c6514573c048d1efa03ed5
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-23 19:18:42 +0200

    CReated File + Initial inputs

---
+++ rocks-game.py
@@ -0,0 +1,7 @@
+print ('--------------------------------------')
+print ('Welcome to Rock-Scissors-Paper Game v1')
+print ('--------------------------------------')
+print()
+
+player1 = input("Enter Player 1 name? ")
+player2 = input("Enter Player 2 name? ")
